c0711d2646449bbdf48a11ab8304cdbea05ffdc8
Andreas Bratfisch <duskman72@gmail.com>
2015-12-15 19:31:00 +0100

    test

--- src/TwigRenderer.php
+++ src/TwigRenderer.php
@@ -1,12 +1,13 @@
 <?php
 
+
 namespace Simplified\TwigBridge;
 use Simplified\Core\ViewRendererInterface;
 
 class TwigRenderer implements ViewRendererInterface {
 	public function render($template, $data = array()) {
-		$loader = new Twig_Loader_Filesystem('/path/to/templates');
-		$twig = new Twig_Environment($loader, array(
+		$loader = new \Twig_Loader_Filesystem('/path/to/templates');
+		$twig = new \Twig_Environment($loader, array(
 				'cache' => '/path/to/compilation_cache',
 		));
 	}
