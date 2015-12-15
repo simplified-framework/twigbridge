0bc5b90335267b998ae69a0c21472cbe711c1764
Andreas Bratfisch <duskman72@gmail.com>
2015-12-15 21:36:13 +0100

    remove extra path

--- src/TwigRenderer.php
+++ src/TwigRenderer.php
@@ -7,7 +7,7 @@ use Simplified\Core\ViewRendererInterface;
 class TwigRenderer implements ViewRendererInterface {
 	private $twig;
 	public function __construct() {
-		$loader = new \Twig_Loader_Filesystem(RESOURCES_PATH . 'views/templates');
+		$loader = new \Twig_Loader_Filesystem(RESOURCES_PATH . 'views');
 		$this->twig = new \Twig_Environment($loader, array(
 				'cache' => APP_PATH . 'cache',
 		));
