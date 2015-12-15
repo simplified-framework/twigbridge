bfa3106a3833200b2cd4f54a27c79172d5ac646e
Andreas Bratfisch <duskman72@gmail.com>
2015-12-15 22:24:21 +0100

    disable cache

--- src/TwigRenderer.php
+++ src/TwigRenderer.php
@@ -7,13 +7,17 @@ use Simplified\Core\ViewRendererInterface;
 class TwigRenderer implements ViewRendererInterface {
 	private $twig;
 	public function __construct() {
+		
+		// TODO load config: set views and cache path from config
+		// set file extension from config
+		
 		$loader = new \Twig_Loader_Filesystem(RESOURCES_PATH . 'views');
 		$this->twig = new \Twig_Environment($loader, array(
-				'cache' => APP_PATH . 'cache',
+				//'cache' => APP_PATH . 'cache',
 		));
 	}
 	
 	public function render($template, $data = array()) {
-		$this->twig->render($template, $data);
+		return $this->twig->render($template, $data);
 	}
 }
