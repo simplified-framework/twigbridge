b000d6a970042b6d79125208a9eb0d4af99e7328
Andreas Bratfisch <duskman72@gmail.com>
2015-12-18 18:54:29 +0100

    set storage path

--- src/TwigRenderer.php
+++ src/TwigRenderer.php
@@ -12,7 +12,7 @@ class TwigRenderer implements ViewRendererInterface {
 		$userConf = Config::getAll('twig');
 		$defaultConf = [
 			'template_path' => RESOURCES_PATH . 'views',
-			'cache_path' => APP_PATH . 'cache',
+			'cache_path' => STORAGE_PATH . 'cache',
 			'use_cache' => true
 		];
 
@@ -20,7 +20,7 @@ class TwigRenderer implements ViewRendererInterface {
 
 		$loader = new \Twig_Loader_Filesystem($config['template_path']);
 		$this->twig = new \Twig_Environment($loader, array(
-				'cache' => $config['use_cache'] ? APP_PATH . 'cache' : false,
+				'cache' => $config['use_cache'] ? STORAGE_PATH . 'cache' : false,
 				"auto_reload" =>  true,
 		));
 	}
