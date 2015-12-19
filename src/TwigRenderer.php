278c6580a07027cbba42ae0329ab7d830fc858f7
Andreas Bratfisch <duskman72@gmail.com>
2015-12-19 21:57:22 +0100

    update interface to support extensions

--- src/TwigRenderer.php
+++ src/TwigRenderer.php
@@ -7,27 +7,35 @@ use Simplified\Core\ViewRendererInterface;
 
 class TwigRenderer implements ViewRendererInterface {
 	private $twig;
+	private static $extensions = array();
 	public function __construct() {
 
-		$userConf = Config::getAll('twig');
-		$defaultConf = [
-			'template_path' => RESOURCES_PATH . 'views',
-			'cache_path' => STORAGE_PATH . 'cache',
-			'use_cache' => true
-		];
+			$userConf = Config::getAll('twig');
+			$defaultConf = [
+				'template_path' => RESOURCES_PATH . 'views',
+				'cache_path' => STORAGE_PATH . 'cache',
+				'use_cache' => true
+			];
 
-		$config = array_merge($defaultConf, $userConf);
+			$config = array_merge($defaultConf, $userConf);
 
-		$loader = new \Twig_Loader_Filesystem($config['template_path']);
-		$this->twig = new \Twig_Environment($loader, array(
+			$loader = new \Twig_Loader_Filesystem($config['template_path']);
+			$this->twig = new \Twig_Environment($loader, array(
 				'cache' => $config['use_cache'] ? STORAGE_PATH . 'cache' : false,
 				"auto_reload" =>  true,
-		));
+			));
+
+		foreach (self::$extensions as $ext) {
+			$this->twig->addExtension($ext);
+		}
 	}
 	
 	public function render($template, $data = array()) {
 		$value = $this->twig->render($template, $data);
 		return $value;
 	}
-	
+
+	public static function registerExtension($ext) {
+		self::$extensions[] = $ext;
+	}
 }
