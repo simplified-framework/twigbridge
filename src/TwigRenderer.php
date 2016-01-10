1de81a273ca17d3f7671de6bfbd6e62080d12698
Andreas Bratfisch <duskman72@gmail.com>
2016-01-10 08:53:58 +0100

    added twig extensions

--- src/TwigRenderer.php
+++ src/TwigRenderer.php
@@ -8,22 +8,23 @@ use Simplified\View\ViewRendererInterface;
 class TwigRenderer implements ViewRendererInterface {
 	private $twig;
 	private static $extensions = array();
+
 	public function __construct() {
+		$userConf = Config::getAll('twig');
+		$defaultConf = [
+			'template_path' => RESOURCES_PATH . 'views',
+			'cache_path' => STORAGE_PATH . 'cache',
+			'use_cache' => true
+		];
 
-			$userConf = Config::getAll('twig');
-			$defaultConf = [
-				'template_path' => RESOURCES_PATH . 'views',
-				'cache_path' => STORAGE_PATH . 'cache',
-				'use_cache' => true
-			];
+		$config = array_merge($defaultConf, $userConf);
 
-			$config = array_merge($defaultConf, $userConf);
-
-			$loader = new \Twig_Loader_Filesystem($config['template_path']);
-			$this->twig = new \Twig_Environment($loader, array(
-				'cache' => $config['use_cache'] ? STORAGE_PATH . 'cache' : false,
-				"auto_reload" =>  true,
-			));
+		$loader = new \Twig_Loader_Filesystem($config['template_path']);
+		$this->twig = new \Twig_Environment($loader, array(
+			'cache' => $config['use_cache'] ? STORAGE_PATH . 'cache' : false,
+			"auto_reload" =>  true,
+		));
+		$this->twig->addExtension(new \Twig_Extensions_Extension_Intl());
 
 		foreach (self::$extensions as $ext) {
 			$this->twig->addExtension($ext);
